d14b1c8360954f3c81df70f5bd778e3c7f9e49dc
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2021-06-16 23:33:20 +0300

    NuGet install

--- Controllers/CPUMetricsController.cs
+++ Controllers/CPUMetricsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,14 +12,24 @@ namespace MetricsManager.Controllers
     [ApiController]
     public class CPUMetricsController : ControllerBase
     {
+        private readonly ILogger<CPUMetricsController> _logger;
+        public CPUMetricsController(ILogger<CPUMetricsController> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "NLog in CpuMetricsController");
+        }
+
+
         [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
         public IActionResult GetCPUMetricsFromAgent([FromRoute] int agentId, [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
         {
+            _logger.LogInformation($"{DateTime.Now} : GetCPUMetricsFromAgent.\nAgent Id: {agentId}\nFrom time: {fromTime}\nTo time: {toTime}\n");
             return Ok();
         }
         [HttpGet("cluster/from/{fromTime}/to/{toTime}")]
         public IActionResult GetCPUMetricsFromAllCluster([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
         {
+            _logger.LogInformation($"{DateTime.Now} : GetCPUMetricsFromAllCluster.\nFrom time: {fromTime}\nTo time: {toTime}\n");
             return Ok();
         }
     }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
+using NLog.Web;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,14 +14,39 @@ namespace MetricsManager
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+                CreateHostBuilder(args).Build().Run();
+            }
+            // отлов всех исключений в рамках работы приложения
+            catch (Exception exception)
+            {
+                //NLog: устанавливаем отлов исключений
+                logger.Error(exception, $"Stopped program because of exception:\n{exception.Message}");
+                throw;
+            }
+            finally
+            {
+                // остановка логера 
+                NLog.LogManager.Shutdown();
+            }
+            // CreateHostBuilder(args).Build().Run();
         }
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();                                                                   // создание провайдеров логирования
+                logging.SetMinimumLevel(LogLevel.Trace);                                                    // устанавливаем минимальный уровень логирования
+            }).UseNLog();                                                                                   // добавляем библиотеку nlog
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //            webBuilder.UseStartup<Startup>();
+        //        });
     }
 }
